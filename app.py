59ad62112c43c7a9f35799c10b51c87b6b99f40a
armodi97 <148710774+armodi97@users.noreply.github.com>
2023-10-29 19:24:56 -0600

    Agregamos graficos y filtros al archivo app.py

--- app.py
+++ app.py
@@ -1,22 +1,75 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import numpy as np
+import subdf
 
-car_data = pd.read_csv("vehicles_us.csv")  # leer los datos
 
-st.sidebar.title("Opciones de Análisis")
+def checkboxes_gen(list):
+    selection = st.sidebar.multiselect("Selecciona:", list)
+    return selection
+
+
+# Creamos un sidebar para añadir algunos filtros
+st.sidebar.header("Filtrar por:")
+
+# Filtro de marca
+brand_check = st.sidebar.checkbox("Marca")
+if brand_check:
+    brands_selected = checkboxes_gen(subdf.brand)
+
+else:
+    brands_selected = subdf.brand.tolist()
+
+
+# Filtro de condición
+condition_check = st.sidebar.checkbox("Condición")
+if condition_check:
+    conditions_selected = checkboxes_gen(subdf.condition)
+
+else:
+    conditions_selected = subdf.condition.tolist()
+
+
+# Filtro de transmisión
+transmission_check = st.sidebar.checkbox("Transmisión")
+if transmission_check:
+    transmission_selected = checkboxes_gen(subdf.transmission)
+
+else:
+    transmission_selected = subdf.transmission.tolist()
+
+
+# Filtro de tipo
+type_check = st.sidebar.checkbox("Tipo")
+if type_check:
+    type_selected = checkboxes_gen(subdf.type)
+
+else:
+    type_selected = subdf.type.tolist()
+
+
+# ejecutar filtro
+confirm_filter = st.sidebar.button("Filtrar")
+if confirm_filter:
+    car_data = subdf.vehicles_df[
+        (subdf.vehicles_df["transmission"].isin(transmission_selected))
+        & (subdf.vehicles_df["type"].isin(type_selected))
+        & (subdf.vehicles_df["model"].str.contains("|".join(brands_selected)))
+        & (subdf.vehicles_df["condition"].isin(conditions_selected))
+    ]
+else:
+    car_data = subdf.vehicles_df
+
+# Creamos un dataframe para mostrar en el dashboard
 st.dataframe(car_data)
 
+# Creamos un grafico de dispersión para mostrar en el dashboard
+fig = px.scatter(car_data, x="odometer", y="price")
+st.plotly_chart(fig)
 
-hist_button = st.button("Construir histograma")  # crear un botón
-if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
-    st.write(
-        "Creación de un histograma para el conjunto de datos de anuncios de venta de coches"
-    )
-
-    # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
-
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+# Crear un histograma
+choices = ["model_year", "price", "odometer", "date_posted", "days_listed"]
+option = st.radio("Selecciona para ver la distribución:", choices)
+hist = px.histogram(car_data, x=option)
+st.plotly_chart(hist)
---
+++ subdf.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# leer los datos
+vehicles_df = pd.read_csv("vehicles_us.csv")
+
+"""extraemos los valores unicos de las columnas con valores cualitativos para usarlos como filtro en el dashboard"""
+
+brand = vehicles_df["model"].str.split(" ").str[0].unique()
+transmission = vehicles_df["transmission"].unique()
+condition = vehicles_df["condition"].unique()
+type = vehicles_df["type"].unique()
+Price = 0
+Odometer = 0
+print()
